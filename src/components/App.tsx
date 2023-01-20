8c7e6c10bcb5f839b43e99383af8385f220329f5
j-mendez <jeff@a11ywatch.com>
2023-01-20 15:52:41 -0500

    chore(api): fix astro api url setting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,9 @@
 import { A11yWatchProvider, setAPIURL } from "@a11ywatch/react-a11ywatch-js";
 import { Program } from './Program';
 
-setAPIURL(import.meta.env.NEXT_PUBLIC_A11YWATCH_API || import.meta.env.API || "http://localhost:3280");
-
 export const App  = () => {
+  setAPIURL(import.meta.env.NEXT_PUBLIC_A11YWATCH_API || import.meta.env.PUBLIC_API || "http://localhost:3280");
+
   return (
     <A11yWatchProvider persist>
       <Program />
